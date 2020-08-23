ce21f60c180ff337a0c00ec806917779583f625b
Coco-jack <ct_jack@163.com>
2020-08-23 23:13:47 +0800

    add new function

--- bigData.js
+++ bigData.js
@@ -1,33 +1,55 @@
 // 监控视频区域滚动
 function scrollVideo() {
-    let speed = 10
-    let scrollBox = document.getElementById('video-detail')
-    let scrollVideo = document.getElementById('video-scroll')
-    let endScroll = document.getElementById('video-scroll-end')
-    endScroll.innerHTML = scrollVideo.innerHTML
-
-    function Marquee() {
-        if (endScroll.offsetWidth - scrollBox.scrollLeft <= 0) {
-            scrollBox.scrollLeft -= scrollVideo.offsetWidth
-        } else {
-            scrollBox.scrollLeft++
-        }
-    }
-
-    let MyMar = setInterval(Marquee, speed)
-    scrollBox.onmouseover = function () {
-        clearInterval(MyMar)
-    }
-    scrollBox.onmouseout = function () {
-        MyMar = setInterval(Marquee, speed)
-    }
+	let speed = 10
+	let scrollBox = document.getElementById('video-detail')
+	let scrollVideo = document.getElementById('video-scroll')
+	let endScroll = document.getElementById('video-scroll-end')
+	endScroll.innerHTML = scrollVideo.innerHTML
+	
+	function Marquee() {
+		if (endScroll.offsetWidth - scrollBox.scrollLeft <= 0) {
+			scrollBox.scrollLeft -= scrollVideo.offsetWidth
+		} else {
+			scrollBox.scrollLeft++
+		}
+	}
+	
+	let MyMar = setInterval(Marquee, speed)
+	scrollBox.onmouseover = function () {
+		clearInterval(MyMar)
+	}
+	scrollBox.onmouseout = function () {
+		MyMar = setInterval(Marquee, speed)
+	}
 }
 
-// 空中花园部分数据轮播(淡入淡出)
-function bannerView() {
-
+// 空中花园部分数据滚动
+function roll(t) {
+    let gardenScroll = document.getElementById("garden-data-detail");
+    let gardenScrollEnd = document.getElementById("garden-data-detail-scroll");
+    let gardenScrollBox = document.getElementById("garden-data-scroll");
+    gardenScrollEnd.innerHTML = gardenScroll.innerHTML;
+    gardenScrollBox.scrollTop = 0;
+    let timer = setInterval(rollStart, t);
+    gardenScrollBox.onmouseover = function () {
+        clearInterval(timer);
+    }
+    gardenScrollBox.onmouseout = function () {
+        timer = setInterval(rollStart, t);
+    }
+}
+function rollStart() {
+    let gardenScroll = document.getElementById("garden-data-detail");
+    let gardenScrollEnd = document.getElementById("garden-data-detail-scroll");
+    let gardenScrollBox = document.getElementById("garden-data-scroll");
+    if (gardenScrollBox.scrollTop >= gardenScroll.scrollHeight) {
+        gardenScrollBox.scrollTop = 0;
+    } else {
+        gardenScrollBox.scrollTop++;
+    }
 }
 
 window.onload = function () {
-    scrollVideo()
+	scrollVideo()
+    roll(50)
 }
